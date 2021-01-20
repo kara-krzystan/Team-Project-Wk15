d0ddca23f06f2a1aa4b8f7c33dc30d1f888a6e84
pat paggi <patpaggi@gmail.com>
2021-01-20 07:35:09 -0700

    start of models

---
+++ models/Appointments.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Appointments extends Model { }
+
+Appointments.init(
+  {
+    //--ID
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //--Time
+    Appointments_time: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    //--appt message
+    Appointments_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    //--appointment type
+    Appointments_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    //--ID of user(foreign key)
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Appointments'
+  }
+);
+
+module.exports = Appointments;
