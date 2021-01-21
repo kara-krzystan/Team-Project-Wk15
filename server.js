e25e3156c48f44a5396bc24fadb0cdd20858a4a9
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-01-20 18:54:47 -0700

    added hbs pages and connections

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require("express");
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// will need to transfer route to controllers, for testing only
+app.get('/', (req, res) => {
+  res.render('login');
+});
 
 //static folder to be decided
 //app.use(express.static('/public'));
@@ -11,5 +22,5 @@ const PORT = process.env.PORT || 3003;
 
 //starts the server
 app.listen(PORT, () => {
-    console.log(`Listening on localhost:${PORT}`);
+  console.log(`Listening on localhost:${PORT}`);
 });
