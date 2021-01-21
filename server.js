a5bd60ec48577efb677359beb21167f011028b24
Ben McRae <benmcrae5@yahoo.com>
2021-01-20 19:41:51 -0700

    I hope this doesn't break anything...

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routes = require('./routes');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -8,6 +9,8 @@ app.use(express.json());
 //app.use(express.static('/public'));
 
 const PORT = process.env.PORT || 3003;
+
+app.use(routes);
 
 //starts the server
 app.listen(PORT, () => {
