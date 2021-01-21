7266fea3b3384c185a1d609d28faf76535e7bdd1
pat paggi <patpaggi@gmail.com>
2021-01-21 11:52:59 -0700

    passport

--- server.js
+++ server.js
@@ -6,62 +6,67 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const env = require('dotenv').config();
 const exphbs = require('express-handlebars');
-
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+const hbs = exphbs.create({
+  helpers: require('./utils/helpers')
+});
 
 // Passport
 app.use(
   session({
-	  	secret: 'rHUyjs6RmVOD06OdOTsVAyUUCxVXaWci',
-	  	resave: true,
-	  	saveUninitialized: true
-  	})
+    secret: 'Super secret secret',
+    cookie: { expires: 10 * 60 * 1000 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  })
 ); // session secret
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Handlebars
-const viewsPath = path.join(__dirname, 'views');
-const layoutsPath = path.join(viewsPath, 'layouts');
-const partialsPath = path.join(viewsPath, 'partials');
-app.set('views', viewsPath);
+// const viewsPath = path.join(__dirname, 'views');
+// const layoutsPath = path.join(viewsPath, 'layouts');
+// const partialsPath = path.join(viewsPath, 'partials');
+// app.set('views', viewsPath);
 
-const exphbsConfig = exphbs.create({
-  defaultLayout: 'main',
-  layoutsDir: layoutsPath,
-  partialsDir: [partialsPath],
-  extname: '.hbs'
-});
 
-app.engine('hbs', exphbsConfig.engine);
-app.set('view engine', '.hbs');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', '.handlebars');
 
 // Models
 const models = require('./models');
 
 // Express static assets
-app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Routes
-const authRoute = require('./controllers/auth.js')(app, passport);
+const authRoute = require('./controllers/login.js')(app, passport);
+//app.use(require('./controllers/'));
 
 // Load passport strategies
 require('./config/passport/passport.js')(passport, models.user);
 
 // Sync Database
-models.sequelize
-  .sync()
-  .then(function() {
+sequelize.sync({ force: false })
+  .then(function () {
     console.log('Database Connected');
 
-    app.listen(3000, function(err) {
-      if (!err) console.log('Connected at http://localhost:3000');
+    app.listen(3001, function (err) {
+      if (!err) console.log('Connected at http://localhost:3001');
       else console.log(err);
     });
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err, 'Error on Database Sync. Please try again!');
   });
