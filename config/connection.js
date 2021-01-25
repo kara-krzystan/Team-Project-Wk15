35a7a7ee638f25cfe57027aa31680655c8f2d440
pat paggi <patpaggi@gmail.com>
2021-01-24 20:37:57 -0700

    google working

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 require('dotenv').config();
 
@@ -11,4 +13,21 @@ const sequelize = process.env.JAWSDB_URL
     port: 3306,
   });
 
+
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+  clientID: '592878734224-1ri6pfkmfslqvnv82ks1bs7n7scaljtn.apps.googleusercontent.com',
+  clientSecret: ' brXAKpG-7Pd0naS-_IEnhPP6',
+  callbackURL: "http://www.example.com/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 module.exports = sequelize;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,50 @@
+
+
+const users = []
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector('#username-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
   }
-  console.log(1)
-  
-  
-  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
-  
-  
+}
+console.log(1)
+
+
+//document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+
+function onSignIn(googleUser) {
+  console.log(googleUser)
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('First Name: ' + profile.getGivenName());
+  console.log('Last Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  /*
+  Still have to make a new user with this
+  */
+
+
+  if (googleUser) {
+    document.location.replace('/');
+  }
+}
