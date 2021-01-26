975c34cd17c68289441725b8d5b511c7c483686f
Ben McRae <benmcrae5@yahoo.com>
2021-01-25 19:06:58 -0700

    add signup route

---
+++ controllers/api/signup-route.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+
+//routes will use /api/SignUp/ {route}
+
+router.post('/', async (req, res) => {
+//get route code here
+res.send("this SignUp route was successful (post)");
+});
+
+router.put('/', async (req, res) => {
+//put route code here
+res.send("this SignUp route was successful (put)");
+});
+
+router.get('/', async (req, res) => {
+//get route code here
+res.send("this SignUp route was successful (get)");
+});
+
+router.delete('/', async (req, res) => {
+//delete route code here
+res.send("this SignUp route was successful (delete)");
+});
+
+module.exports = router;
