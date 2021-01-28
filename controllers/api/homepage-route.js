540b716aa2bda7f1560eccf05ebd331e59af872b
pat paggi <patpaggi@gmail.com>
2021-01-27 17:09:33 -0700

    appointment

--- controllers/api/homepage-route.js
+++ controllers/api/homepage-route.js
@@ -1,8 +1,36 @@
 const router = require('express').Router();
 const { Appointment, Timeblock, User } = require('../../models');
 
-router.get('/', async (req, res) => {
-  res.render('homepage');
+router.get('/', (req, res) => {
+  console.log('======================');
+  Appointment.findAll({
+    include: [
+      {
+        model: Appointment,
+        attributes: ['id', 'Appointment_time', 'Appointments_date', 'Appointments_day', 'Appointments_text', 'user_id'],
+        include: {
+          model: User,
+          attributes: ['user']
+        }
+      },
+      {
+        model: User,
+        attributes: ['user']
+      }
+    ]
+  })
+    .then(dbAppointmentData => {
+      const posts = dbAppointmentData.map(appointment => appointment.get({ plain: true }));
+      console.log(appointments)
+      res.render('homepage', {
+        appointments,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 
