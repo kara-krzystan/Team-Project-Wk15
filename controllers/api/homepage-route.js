8c2df5260bf32f46773bec945982686ca5ed7008
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-01-31 14:09:39 -0700

    fix 5000

--- controllers/api/homepage-route.js
+++ controllers/api/homepage-route.js
@@ -2,38 +2,36 @@ const router = require('express').Router();
 const { Appointment, Timeblock, User } = require('../../models');
 
 router.get('/', async (req, res) => {
-  try{
-  console.log(req.session.user_id);
-  const { user_id } = req.session;
-    let appointments = user_id 
+  try {
+    console.log(req.session.user_id);
+    const { user_id } = req.session;
+    let appointments = user_id
       ? await Appointment.findAll({
-        where: {
-          user_id: req.session.user_id,
-        },
-        include: [
-          {
-            model: User,
-            attributes: ['username'],
+          where: {
+            user_id: req.session.user_id,
           },
-          Timeblock,
-        ],
-      }).then ( appointments => 
-        appointments?.map(appointment =>
-          appointment.get({ plain: true })
+          include: [
+            {
+              model: User,
+              attributes: ['username'],
+            },
+            Timeblock,
+          ],
+        }).then(appointments =>
+          appointments.map(appointment => appointment.get({ plain: true }))
         )
-      ) 
-      : null ;
-  
+      : null;
+
     // console.log(appointments)
-    
+
     res.render('homepage', {
       appointments,
       loggedIn: true,
     });
-} catch (err) {
-  console.log(err);
-  res.status(500).json(err);
-}
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
